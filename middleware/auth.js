97ea7ce42a1233088ce16653db15b6e3d0ddffc6
mumar389 <mohammadumar389@gmail.com>
2024-04-17 09:29:51 +0530

    testing Auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,13 +3,18 @@ import { UnAuthenticatedError } from '../errors/index.js';
 
 const auth = async (req, res, next) => {
   const token = req.cookies.token;
+  console.log("reach1");
   if (!token) {
     throw new UnAuthenticatedError('Authentication Invalid');
   }
   try {
+    console.log("reach2");
     const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("reach3",payload);
     const testUser = payload.userId === '63628d5d178e918562ef9ce8';
+    console.log("reach4");
     req.user = { userId: payload.userId, testUser };
+    console.log("reach5");
     next();
   } catch (error) {
     throw new UnAuthenticatedError('Authentication Invalid');
